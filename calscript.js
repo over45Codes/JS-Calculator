0c7bb7ded038b60792299cd8f6e31a7c8b464ed2
Grace Castaneda <griseldacastaneda2013@gmail.com>
2020-12-03 21:58:30 -0800

    first js commit added all elements and eventListeners

---
+++ calscript.js
@@ -0,0 +1,94 @@
+let memoryButtons = document.querySelectorAll(".memory-btn");
+let numbersButtons = document.querySelectorAll(".number-btn");
+let operatorButtons = document.querySelectorAll(".operator");
+
+const clearButton = document.getElementById("clear");
+clearButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  console.log("clicked", clearButton);
+});
+
+const equalButton = document.getElementById("equal");
+equalButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  console.log("clicked", equalButton);
+});
+
+memoryButtons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    event.preventDefault();
+    console.log("clicked", button);
+  });
+});
+
+numbersButtons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    event.preventDefault();
+    console.log("clicked", button);
+  });
+});
+
+operatorButtons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    event.preventDefault();
+    console.log("clicked", button);
+  });
+});
+
+const calcDisplay = document.getElementById("display");
+const calculator = {
+  displayOutput: "",
+};
+
+function renderDisplay(button) {
+  button.addEventListener("click", () => {
+    calcDisplay.appendNumber(button.textContent);
+    calculator.renderDisplay();
+  });
+}
+
+// / TEST CASE
+//THIS IS WHERE I AM AT RENDERING - GC 9:57PM 12/03
+// Description: Adding 1 + 2 = 3
+// 1.) Click on the number one button
+// EXPECTATION: Show 1 in the display
+// ACTUAL: ???
+
+// 2.) Click on the plus button
+// EXPECTATION: Nothing. (Still showing 1 in the display)
+// ACTUAL: ???
+
+// 3.) Click on the number two button
+// EXPECTATION: Show 2 in the display
+// ACTUAL: ???
+
+// 4.) Click on the equals button
+// EXPECTATION: Show 3 in the display
+// ACTUAL: ???
+// ===========================================
+// // TEST CASE
+// // Description: Adding 11 + 22 = 33
+
+// 1.) Click on the number one button
+// EXPECTATION: Show 1 in the display
+// ACTUAL: ???
+
+// 2.) Click on the number one button
+// EXPECTATION: Show 11 in the display
+// ACTUAL: ???
+
+// 3.) Click on the plus button
+// EXPECTATION: Nothing. (Still showing 11 in the display)
+// ACTUAL: ???
+
+// 4.) Click on the number two button
+// EXPECTATION: Show 2 in the display
+// ACTUAL: ???
+
+// 5.) Click on the number two button
+// EXPECTATION: Show 22 in the display
+// ACTUAL: ???
+
+// 6.) Click on the equals button
+// EXPECTATION: Show 33 in the display
+// ACTUAL: ???
